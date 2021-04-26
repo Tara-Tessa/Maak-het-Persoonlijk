d91caf6ad81615303ae0254b76d4ef198543f53c
Tara-Tessa <tara-tessa.tesch@student.howest.be>
2021-04-26 16:29:23 +0200

    fallback true #13

--- pages/cakes/[id].js
+++ pages/cakes/[id].js
@@ -5,6 +5,8 @@ import Layout from "../../components/layout";
 import copy from 'copy-to-clipboard'
 
 const Cake = (cake) => {
+  if (!cake) return <div>Loading...</div>
+
     const [candles, setCandles] = useState("");
     const [meringue, setMeringue] = useState("");
     const [icing, setIcing] = useState("");
@@ -116,7 +118,7 @@ export const getStaticPaths = async () => {
 
   return {
     paths,
-    fallback: false,
+    fallback: true,
   };
 }
 
